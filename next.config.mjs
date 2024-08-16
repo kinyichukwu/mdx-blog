95d214cdbf282ca319e1766a24842fb1227d54e9
kinyichukwu <kinyichukwuose@gmail.com>
2024-08-16 12:25:24 +0100

    Native Docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from "@next/mdx";
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, if needed
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+};
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
